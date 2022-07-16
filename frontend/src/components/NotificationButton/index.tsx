700b6663ded3fc1bb66141f8808d7681f4f43dc2
Tiago Lopes <tiagoltavares2002@gmail.com>
2022-07-15 22:10:08 -0300

    Implementando envio de notificações
    - Utilização do axios para fazer um GET na rota /sales/{id}/notification
    - Criação de lógica para envio do SMS

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,21 @@
+import axios from 'axios'
 import notificationIcon from '../../assets/img/notification-icon.svg'
+import { BASE_URL } from '../../utils/request'
 import './styles.css'
 
-export default function NotificationButton() {
+type Props = {
+    saleId: number
+}
+
+function handleClick(id: number) {
+    axios.get(`${BASE_URL}/sales/${id}/notification`)
+        .then(() => console.log('Notificação enviada'))
+        .catch(err => console.log('Erro', err))
+}
+
+export default function NotificationButton({ saleId }: Props) {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={notificationIcon} alt="Notificar" />
         </div>
     )
